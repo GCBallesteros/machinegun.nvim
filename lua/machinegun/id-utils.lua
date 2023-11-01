37db7a3aac7bc561e1aa942402f7b83e033077de
GCBallesteros <dev+github@maxwellrules.com>
2023-11-01 08:12:37 +0100

    refactor: Move `get_machine_name` to `id-utils`

--- lua/machinegun/id-utils.lua
+++ lua/machinegun/id-utils.lua
@@ -39,4 +39,35 @@ M.get_user = function()
   end
 end
 
+
+--- Returns the name in the config associated with this machine
+---
+---@param machines table A table of pairs `machine_name = machine_id`.
+---  To retrieve the `machine_id` you will have to run the `get_machine_id` function
+---  or run one of the following commands depending on the OS you are running: -    - Mac:
+---    - Mac: `ioreg -d2 -c IOPlatformExpertDevice | awk -F\" '/IOPlatformUUID/{print $(NF-1)}' | shasum -a 1 | cut -f 1 -d  " "`
+---    - Linux: `cat /etc/machine-id | shasum -a 1 | cut -f 1 -d  " "`
+---
+---  The machines table doesn't need to include the whole hash. It will do a prefix
+---  match so you can just put in there the first few characters of the hash. Seven or
+---  eight are for most scenarios enough to avoid collisions and not make the task to
+---  onerous.
+---@return string|boolean
+M.get_machine_name = function(machines)
+  local machine_id = M.get_machine_id()
+  if not machine_id then
+    return false
+  end
+
+  for machine_name, mid in pairs(machines) do
+    local check_id = vim.fn.match(machine_id, "^" .. mid) == 0
+    if check_id then
+      return machine_name
+    end
+  end
+
+  -- Didn't find the machine
+  return false
+end
+
 return M
